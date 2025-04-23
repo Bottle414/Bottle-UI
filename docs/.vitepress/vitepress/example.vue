9312ee5a7e65131ce9625190c36565ebf74c4f5a
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-04-23 15:14:29 +0800

    feat(docs/example): add pulldown support for code presentation

--- docs/.vitepress/vitepress/example.vue
+++ docs/.vitepress/vitepress/example.vue
@@ -1,16 +1,28 @@
 <template>
-    <div class='example'>
+    <div class='example' :class="{ 'example-active' : pull}">
         <div class="content">
             <slot></slot>
         </div>
         <div class="foot">
-            <sapn>Copy</sapn>
-            <span>Code</span>
+            <sapn title="复制代码">Copy</sapn>
+            <span title="查看源代码" @click="handlePull">Code</span>
         </div>
+    </div>
+    <div class="pulldown" :class="{ 'pulldown-active' : pull}">
+        <slot name="text">
+            等待更新
+        </slot>
     </div>
 </template>
 
 <script lang='ts' setup>
+    import { ref } from 'vue'
+    let pull = ref(false)
+
+    function handlePull(){
+        pull.value = !pull.value
+    }
+
     defineOptions({
         name: 'example'
     })
@@ -18,13 +30,24 @@
 
 <style scoped>
     .example {
+        position: relative;
         display: flex;
         flex-direction: column;
         border: 1px solid #ddd;
-        border-radius: 10px;
+        border-radius: 5px;
+        overflow: hidden;
+    }
+
+    .example-active {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
     }
 
     .content {
+        display: flex;
+        align-items: baseline;
+        flex-wrap: wrap;
+        gap: 30px;
         padding: 5%;
     }
 
@@ -33,5 +56,24 @@
         display: flex;
         gap: 5%;
         justify-content: right;
+        border-top: 1px solid #ddd;
+        font-size: 14px;
+    }
+
+    .pulldown {
+        width: 100%;
+        position: relative;
+        /*  bottom: -20%;/* 相对于父级的位置 */
+        /* left: 0; */
+        top: 0px;
+        background: #eee;
+        transform: scaleY(0);
+        transform-origin: top;
+        transition: transform .3s;
+        overflow: hidden;
+    }
+
+    .pulldown-active {
+        transform: scaleY(1);
     }
 </style>

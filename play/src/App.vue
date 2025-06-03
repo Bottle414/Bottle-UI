559c1fb0295a945cd05c8eb0fc15063066046e23
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-06-03 22:03:24 +0800

    feat(theme-chalk): add custom style support for card

--- play/src/App.vue
+++ play/src/App.vue
@@ -8,10 +8,17 @@
     <BUpload multiple :before-upload="handleBeforeUpload" action="http://localhost:3000/upload" showList listType="image">
         <BButton size="large">Upload</BButton>
     </BUpload>
+    <br/>
+    <BCard shadow="always">
+        why
+        <BButton>
+            How about the buton
+        </BButton>
+    </BCard>
 </template>
 
 <script lang='ts' setup>
-    import { BUpload, BButton } from '@bottle-ui/components'
+    import { BUpload, BButton, BCard } from '@bottle-ui/components'
     function handleBeforeUpload(){
         console.log('upload')
         return true
@@ -19,4 +26,8 @@
 </script>
 
 <style scoped>
+    .b-card__body {
+        display: flex;
+        flex-direction: column;
+    }
 </style>
